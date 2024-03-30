a0109d769b173c139bc12fa26f08e389f3be9dc1
jinsuklim <94436897+jinsuklim@users.noreply.github.com>
2024-03-31 00:24:55 +0800

    Add struct_iterable_internal import

--- struct_iterable_derive/src/lib.rs
+++ struct_iterable_derive/src/lib.rs
@@ -3,6 +3,7 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use struct_iterable_internal::Iterable;
 
 /// The `Iterable` proc macro.
 ///
